788962a0c4b3fbbb9c20c7bb726fbe010285c335
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-03 19:14:51 +0200

    print elemnt in page

--- index.php
+++ index.php
@@ -48,48 +48,82 @@ $the_godfather->addActor('Robert Duvall');
             <div class="card">
                 <ul>
                     <li>
-                        <span><strong>Titolo:</strong></span>
-                        <span>Inception</span>
+                        <h3><?php echo $inception->title ?></span></h3>
                     </li>
                     <li>
                         <span><strong>Regista:</strong></span>
-                        <span>Inception</span>
+                        <span><?php echo $inception->director ?></span>
                     </li>
                     <li>
                         <span><strong>Attori principali:</strong></span>
-                        <span>Inception</span>
+                        <?php foreach ($inception->mainActors as $actor): ?>
+                            <span><?php echo $actor ?> -</span>
+                        <?php endforeach; ?>
                     </li>
                     <li>
                         <span><strong>Lingua originale:</strong></span>
-                        <span>Inception</span>
+                        <span><?php echo $inception->language ?></span>
                     </li>
                     <li>
                         <span><strong>Genere:</strong></span>
-                        <span>Inception</span>
+                        <?php foreach ($inception->genre as $gen): ?>
+                            <span><?php echo $gen ?> -</span>
+                        <?php endforeach; ?>
                     </li>
                     <li>
                         <span><strong>Rating:</strong></span>
-                        <span>Inception</span>
+                        <span><span><?php echo $inception->rating ?></span></span>
+                    </li>
+                </ul>
+            </div>
+            <div class="card">
+                <ul>
+                    <li>
+                        <h3><?php echo $spirited_away->title ?></h3>
+                    </li>
+                    <li>
+                        <span><strong>Regista:</strong></span>
+                        <span><?php echo $spirited_away->director ?></span>
+                    </li>
+                    <li>
+                        <span><strong>Attori principali:</strong></span>
+                        <?php foreach ($spirited_away->mainActors as $actor): ?>
+                            <span><?php echo $actor ?> -</span>
+                        <?php endforeach; ?>
+                    </li>
+                    <li>
+                        <span><strong>Lingua originale:</strong></span>
+                        <span><?php echo $spirited_away->language ?></span>
+                    </li>
+                    <li>
+                        <span><strong>Genere:</strong></span>
+                        <?php foreach ($spirited_away->genre as $gen): ?>
+                            <span><?php echo $gen ?> -</span>
+                        <?php endforeach; ?>
+                    </li>
+                    <li>
+                        <span><strong>Rating:</strong></span>
+                        <span><span><?php echo $spirited_away->rating ?></span></span>
                     </li>
                 </ul>
             </div>
         </div>
     </div>
-    <!-- Oggi pomeriggio ripassate i primi concetti di classe, variabili e metodi d’istanza che abbiamo visto stamattina e create un file index.php in cui:
-    - è definita una classe ‘Movie’
-    => all’interno della classe sono dichiarate delle variabili d’istanza
-    => all’interno della classe è definito un costruttore
-    => all’interno della classe è definito almeno un metodo
-    - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
-    Bonus 1:
-    Modificare la classe Movie in modo che accetti piú di un genere.
-    Bonus 2:
-    Creare un layout completo per stampare a schermo una lista di movies.
-    Facciamo attenzione all’organizzazione del codice, suddividendolo in appositi file e cartelle. Possiamo ad esempio organizzare il codice
-    - creando un file dedicato ai dati (tipo le array di oggetti) che potremmo chiamare db.php
-    - mettendo ciascuna classe nel proprio file e magari raggruppare tutte le classi in una cartella dedicata che possiamo chiamare Models/
-    - organizzando il layout dividendo la struttura ed i contenuti in file e parziali dedicati.
-    Siate creativi e siete liberi di aggiungere metodi o proprietà a piacimento -->
+    // <!-- Oggi pomeriggio ripassate i primi concetti di classe, variabili e metodi d’istanza che abbiamo visto stamattina e create un file index.php in cui:
+    // - è definita una classe ‘Movie’
+    // => all’interno della classe sono dichiarate delle variabili d’istanza
+    // => all’interno della classe è definito un costruttore
+    // => all’interno della classe è definito almeno un metodo
+    // - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+    // Bonus 1:
+    // Modificare la classe Movie in modo che accetti piú di un genere.
+    // Bonus 2:
+    // Creare un layout completo per stampare a schermo una lista di movies.
+    // Facciamo attenzione all’organizzazione del codice, suddividendolo in appositi file e cartelle. Possiamo ad esempio organizzare il codice
+    // - creando un file dedicato ai dati (tipo le array di oggetti) che potremmo chiamare db.php
+    // - mettendo ciascuna classe nel proprio file e magari raggruppare tutte le classi in una cartella dedicata che possiamo chiamare Models/
+    // - organizzando il layout dividendo la struttura ed i contenuti in file e parziali dedicati.
+    // Siate creativi e siete liberi di aggiungere metodi o proprietà a piacimento -->
 </body>
 
 </html>
