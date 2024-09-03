c173f75b2f26a631f075d99da9ce3b72aad161db
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-03 17:03:22 +0200

    add movie list

--- Model/Movie.php
+++ Model/Movie.php
@@ -6,12 +6,14 @@ class Movie
     public $director;
     public $mainActors = [];
     public $language;
+    public $genre = [];
 
-    function __construct($_title, $_director, $_mainActors, $_language)
+    function __construct($_title, $_director, $_mainActors, $_language, $_genre)
     {
         $this->title = $_title;
         $this->director = $_director;
         $this->mainActors = $_mainActors;
         $this->language = $_language;
+        $this->genre = $_genre;
     }
 }
--- index.php
+++ index.php
@@ -4,21 +4,23 @@
 
 require_once __DIR__ . '/Model/Movie.php';
 
-/*Oggi pomeriggio ripassate i primi concetti di classe, variabili e metodi d’istanza che abbiamo visto stamattina e create un file index.php in cui:
- - è definita una classe ‘Movie’
-   => all’interno della classe sono dichiarate delle variabili d’istanza
-   => all’interno della classe è definito un costruttore
-   => all’interno della classe è definito almeno un metodo
-- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
-Bonus 1:
-Modificare la classe Movie in modo che accetti piú di un genere.
-Bonus 2:
-Creare un layout completo per stampare a schermo una lista di movies.
-Facciamo attenzione all’organizzazione del codice, suddividendolo in appositi file e cartelle. Possiamo ad esempio organizzare il codice
-- creando un file dedicato ai dati (tipo le array di oggetti) che potremmo chiamare db.php
-- mettendo ciascuna classe nel proprio file e magari raggruppare tutte le classi in una cartella dedicata che possiamo chiamare Models/
-- organizzando il layout dividendo la struttura ed i contenuti in file e parziali dedicati.
-Siate creativi e siete liberi di aggiungere metodi o proprietà a piacimento*/
+//lista di film 
+
+$inception = new Movie('Inception', 'Christopher Nolan', ['Leonardo DiCaprio', 'Joseph Gordon-Levitt', 'Ellen Page'], 'Inglese', ['Fantascienza', 'Thriller']);
+
+$la_vita_è_bella = new Movie('La vita è bella', 'Roberto Benigni', ['Roberto Benigni', ' Nicoletta Braschi,', 'Giorgio Cantarini'], 'Italiano', ['Dramma', 'Commedia']);
+
+$parasite = new Movie('Parasite', 'Bong Joon-ho', ['Song Kang-ho', 'Lee Sun-kyun', 'Cho Yeo-jeong'], 'Coreano', ['Thriller', 'Drammatico']);
+
+$amelie = new Movie('Amélie', 'Jean-Pierre Jeunet', ['Audrey Tautou', 'Mathieu Kassovitz', 'Rufus'], 'Francese', ['Commedia romantica', 'Fantastico']);
+
+$the_godfather = new Movie('The Godfather', 'Francis Ford Coppola', ['Marlon Brando', 'Al Pacino', 'James Caan'], 'Inglese', ['Drammatico', 'Crime']);
+
+$spirited_away = new Movie('Spirited Away', 'Hayao Miyazaki', ['Rumi Hiiragi', 'Miyu Irino', 'Mari Natsuki'], 'Giapponese', ['Animazione', 'Avventura', 'Fantastico']);
+
+var_dump($inception, $la_vita_è_bella, $parasite, $amelie, $the_godfather, $spirited_away);
+
+
 
 ?>
 
@@ -32,7 +34,21 @@ Siate creativi e siete liberi di aggiungere metodi o proprietà a piacimento*/
 </head>
 
 <body>
-
+    <!-- Oggi pomeriggio ripassate i primi concetti di classe, variabili e metodi d’istanza che abbiamo visto stamattina e create un file index.php in cui:
+    - è definita una classe ‘Movie’
+    => all’interno della classe sono dichiarate delle variabili d’istanza
+    => all’interno della classe è definito un costruttore
+    => all’interno della classe è definito almeno un metodo
+    - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+    Bonus 1:
+    Modificare la classe Movie in modo che accetti piú di un genere.
+    Bonus 2:
+    Creare un layout completo per stampare a schermo una lista di movies.
+    Facciamo attenzione all’organizzazione del codice, suddividendolo in appositi file e cartelle. Possiamo ad esempio organizzare il codice
+    - creando un file dedicato ai dati (tipo le array di oggetti) che potremmo chiamare db.php
+    - mettendo ciascuna classe nel proprio file e magari raggruppare tutte le classi in una cartella dedicata che possiamo chiamare Models/
+    - organizzando il layout dividendo la struttura ed i contenuti in file e parziali dedicati.
+    Siate creativi e siete liberi di aggiungere metodi o proprietà a piacimento -->
 </body>
 
 </html>
