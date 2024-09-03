90fa6f835f06283277d706264969ef47888649fb
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-03 18:25:39 +0200

    add function for rating

--- Model/Movie.php
+++ Model/Movie.php
@@ -7,18 +7,34 @@ class Movie
     public $mainActors = [];
     public $language;
     public $genre = [];
+    public $italian_title;
+    public $rating;
+    public $for_who;
 
-    function __construct($_title, $_director, $_mainActors, $_language, $_genre)
+    function __construct($_title, $_director, $_mainActors, $_language, $_genre, $_rating)
     {
         $this->title = $_title;
         $this->director = $_director;
         $this->mainActors = $_mainActors;
         $this->language = $_language;
         $this->genre = $_genre;
+        $this->rating = $_rating;
     }
 
     public function addActor($_actor)
     {
         $this->mainActors[] = $_actor;
+    }
+
+    public function forKids($rating)
+    {
+        if ($rating == 'G') {
+            $this->for_who = 'Il film è adatto a tutte le età';
+        } else if ($rating == 'PG' || $rating == 'PG-13') {
+            $this->for_who = 'Si consiglia la supervisione dei genitori';
+        } else {
+            $this->for_who = 'Il film è riservato a un pubblico adulto';
+        }
+        return $this->for_who;
     }
 }
--- index.php
+++ index.php
@@ -6,17 +6,17 @@ require_once __DIR__ . '/Model/Movie.php';
 
 //lista di film 
 
-$inception = new Movie('Inception', 'Christopher Nolan', ['Leonardo DiCaprio', 'Joseph Gordon-Levitt', 'Ellen Page'], 'Inglese', ['Fantascienza', 'Thriller']);
+$inception = new Movie('Inception', 'Christopher Nolan', ['Leonardo DiCaprio', 'Joseph Gordon-Levitt', 'Ellen Page'], 'Inglese', ['Fantascienza', 'Thriller'], 'PG-13');
 
-$la_vita_è_bella = new Movie('La vita è bella', 'Roberto Benigni', ['Roberto Benigni', ' Nicoletta Braschi,', 'Giorgio Cantarini'], 'Italiano', ['Dramma', 'Commedia']);
+$la_vita_è_bella = new Movie('La vita è bella', 'Roberto Benigni', ['Roberto Benigni', ' Nicoletta Braschi,', 'Giorgio Cantarini'], 'Italiano', ['Dramma', 'Commedia'], 'PG-13');
 
-$parasite = new Movie('Parasite', 'Bong Joon-ho', ['Song Kang-ho', 'Lee Sun-kyun', 'Cho Yeo-jeong'], 'Coreano', ['Thriller', 'Drammatico']);
+$parasite = new Movie('Parasite', 'Bong Joon-ho', ['Song Kang-ho', 'Lee Sun-kyun', 'Cho Yeo-jeong'], 'Coreano', ['Thriller', 'Drammatico'], 'R');
 
-$amelie = new Movie('Amélie', 'Jean-Pierre Jeunet', ['Audrey Tautou', 'Mathieu Kassovitz', 'Rufus'], 'Francese', ['Commedia romantica', 'Fantastico']);
+$amelie = new Movie('Amélie', 'Jean-Pierre Jeunet', ['Audrey Tautou', 'Mathieu Kassovitz', 'Rufus'], 'Francese', ['Commedia romantica', 'Fantastico'], 'R');
 
-$the_godfather = new Movie('The Godfather', 'Francis Ford Coppola', ['Marlon Brando', 'Al Pacino', 'James Caan'], 'Inglese', ['Drammatico', 'Crime']);
+$the_godfather = new Movie('The Godfather', 'Francis Ford Coppola', ['Marlon Brando', 'Al Pacino', 'James Caan'], 'Inglese', ['Drammatico', 'Crime'], 'R');
 
-$spirited_away = new Movie('Spirited Away', 'Hayao Miyazaki', ['Rumi Hiiragi', 'Miyu Irino', 'Mari Natsuki'], 'Giapponese', ['Animazione', 'Avventura', 'Fantastico']);
+$spirited_away = new Movie('Spirited Away', 'Hayao Miyazaki', ['Rumi Hiiragi', 'Miyu Irino', 'Mari Natsuki'], 'Giapponese', ['Animazione', 'Avventura', 'Fantastico'], 'PG');
 
 // aggiungo il nome di altri attori principali
 
